df31effafad13c6eef7a4ebddb894c82f38a70ac
Benz56//Benjamin <bemmamin@live.dk>
2018-09-21 00:20:36 +0200

    Spacing fix.

--- src/assignments/modules/module005/Module005.java
+++ src/assignments/modules/module005/Module005.java
@@ -27,13 +27,9 @@ public class Module005 extends AModule {
         addAssignment("6.22", scanner -> new Module005_6_22(scanner).runAssignment());
         addAssignment("6.25", scanner -> new Module005_6_25(scanner).runAssignment());
         addAssignment("6.28", scanner -> new Module005_6_28(scanner).runAssignment());
-
         addAssignment("D.2", scanner -> System.out.printf("The sum of the circumferences is %.2f", Stream.of(3, 7).mapToDouble(radius -> Math.PI * Math.pow(radius, 2)).sum()));
-
         addAssignment("D.5", scanner -> new Module005_D_5(scanner).runAssignment());
-
         addAssignment("D.7", scanner -> new GameOfLife(scanner).runAssignment());
-
         addAssignment("D.8", scanner -> new Module005_D_8(scanner).runAssignment());
     }
 }
