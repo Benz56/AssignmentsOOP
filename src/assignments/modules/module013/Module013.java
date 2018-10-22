c211b300832345cbab5ce25c11fd8386d696e519
Benjamin Staugaard <bemmamin@live.dk>
2018-10-22 21:42:13 +0200

    Module 013

--- src/assignments/modules/module013/Module013.java
+++ src/assignments/modules/module013/Module013.java
@@ -5,14 +5,15 @@
  */
 package assignments.modules.module013;
 
+import assignments.ScannerUtil;
 import assignments.modules.AModule;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -26,14 +27,19 @@ public class Module013 extends AModule {
 
     @Override
     public void loadModuleTasks() {
-        addAssignment("run", scanner -> {
+        addAssignment("1", scanner -> {
             final String input = "1,2,red,blue";
-            
-            final File csvFile = new File("C:\\Users\\bemma\\Desktop\\Uni\\OOP\\twocolors.csv"); // Create the File Object.
-            if (!csvFile.exists()) {                                                // If the file doesn't exist, create it.
+
+            final File csvFile;
+            try {
+                csvFile = getFile();
+            } catch (IOException ex) {
+                System.out.println("Something terrible went wrong! Aborting");  // Print error message.
+                return;                                                         // Return i.e. abort the rest.
+            }
+
+            if (csvFile.length() == 0) {                                            // Check if the file is empty.
                 try {
-                    csvFile.createNewFile();                                        // Create the file. (Throws IOException)
-                    
                     final PrintWriter printWriter = new PrintWriter(csvFile);       // Create the PrintWriter Object. (Throws FileNotFoundException; subclass of IOException)
                     printWriter.println(input);                                     // Print/write the String to the File.
                     printWriter.close();                                            // Close the resource.
@@ -57,5 +63,118 @@ public class Module013 extends AModule {
 
             fileScanner.close();  // Close the scanner.
         });
+
+        addAssignment("2", scanner -> {
+            final File csvFile;
+            try {
+                csvFile = getFile();
+            } catch (IOException ex) {
+                System.out.println("Something terrible went wrong! Aborting");
+                return;
+            }
+
+            if (csvFile.length() == 0) {
+                System.out.println("The File is empty...");
+                return;
+            }
+
+            final Scanner csvScanner;
+            try {
+                csvScanner = new Scanner(csvFile).useDelimiter(",");
+            } catch (FileNotFoundException ex) {
+                System.out.println("Something terrible went wrong! Aborting");
+                return;
+            }
+
+            List<String> entries = new ArrayList<>();
+            int i = 0;
+            while (csvScanner.hasNext()) {
+                i++;
+                String entry = csvScanner.next();
+                entries.add(entry);
+                System.out.printf("%d%-3s%s", i, ".", entry);
+                System.out.println("");
+            }
+            csvScanner.close();
+
+            System.out.println("Which entry would you like to delete?");
+            int index;
+            do {
+                index = ScannerUtil.getInteger(scanner, "Please input a number between 1 and " + i + " (both inclusive)");
+            } while (index < 1 || index > i);
+
+            entries.remove(index - 1);
+
+            try {
+                final PrintWriter printWriter = new PrintWriter(csvFile);
+                printWriter.print(String.join(",", entries));
+                printWriter.close();
+            } catch (IOException ex) {
+                System.out.println("Something terrible went wrong! Aborting");
+                return;
+            }
+        });
+
+        addAssignment("3", scanner -> {
+            final File csvFile;
+            try {
+                csvFile = getFile();
+            } catch (IOException ex) {
+                System.out.println("Something terrible went wrong! Aborting");
+                return;
+            }
+
+            final Scanner csvScanner;
+            try {
+                csvScanner = new Scanner(csvFile).useDelimiter(",");
+            } catch (FileNotFoundException ex) {
+                System.out.println("Something terrible went wrong! Aborting");
+                return;
+            }
+
+            List<String> entries = new ArrayList<>();
+            int i = 0;
+            while (csvScanner.hasNext()) {
+                i++;
+                String entry = csvScanner.next();
+                entries.add(entry);
+                System.out.printf("%d%-3s%s", i, ".", entry);
+                System.out.println("");
+            }
+            csvScanner.close();
+
+            System.out.println("Where would you like to add your entry?");
+            int index;
+            do {
+                index = ScannerUtil.getInteger(scanner, "Please input a number between 1 and " + i + " (both inclusive)");
+            } while (index < 1 || index > i);
+
+            System.out.println("Please input what you would like to add to the CSV file. (Leave out the comma)");
+            String input = scanner.next();
+
+            entries.add(index - 1, input);
+
+            try {
+                final PrintWriter printWriter = new PrintWriter(csvFile);
+                printWriter.print(String.join(",", entries));
+                printWriter.close();
+            } catch (IOException ex) {
+                System.out.println("Something terrible went wrong! Aborting");
+            }
+        });
+    }
+
+    /**
+     * Returns the CSV File. If it doesn't exist it is created.
+     *
+     * @return the CSV File.
+     * @throws IOException if the File couldn't be created.
+     */
+    public File getFile() throws IOException {
+        final File csvFile = new File("C:\\Users\\bemma\\Desktop\\Uni\\OOP\\twocolors.csv");  // Create the File Object.
+        if (!csvFile.exists()) {                                                              // If the file doesn't exist, create it.
+            csvFile.createNewFile();                                                          // Create the file.
+        }
+        return csvFile;
     }
 }
